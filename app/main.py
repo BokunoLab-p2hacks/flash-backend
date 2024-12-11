b1a5f047d1f2d3f98dcd4875a166487f15d662dd
Seiga-Kimura <b1021138@fun.ac.jp>
2024-12-11 17:05:36 +0900

    modelの読み込む時のパスを変更

--- app/main.py
+++ app/main.py
@@ -6,8 +6,7 @@ import google.generativeai as genai
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import numpy as np
 import random
-from pydantic import BaseModel  # 追加
-#from response_dict import response_dict
+from pydantic import BaseModel
 
 # 環境変数の読み込み
 load_dotenv()
@@ -21,12 +20,16 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-
 # モデルとトークナイザーの読み込み
-tokenizer = AutoTokenizer.from_pretrained("model_checkpoint")
-model_h = AutoModelForSequenceClassification.from_pretrained("model_checkpoint")
+tokenizer = AutoTokenizer.from_pretrained("../model_checkpoint")
+model_h = AutoModelForSequenceClassification.from_pretrained("../model_checkpoint")
 
 emotion_names_jp = ['喜び', '悲しみ', '期待', '驚き', '怒り', '恐れ', '嫌悪', '信頼']
+
+# Softmax 関数の実装
+def np_softmax(x):
+    exp_x = np.exp(x - np.max(x))
+    return exp_x / exp_x.sum()
 
 # 感情分析関数
 def analyze_emotion(text: str):
@@ -38,12 +41,7 @@ def analyze_emotion(text: str):
     second_emotion = emotion_names_jp[np.argsort(prob)[-2]]
     probs = []
     for i in range(len(prob)):
-        probs = probs.append((emotion_names_jp[i], prob[i]))
-    # 値が0.5以上の感情を取得し、配列に格納（配列の中身は感情：値セット）
-    #emotions = []
-    #for i in range(len(prob)):
-    #    if prob[i] >= 0.4:
-    #        emotions.append((emotion_names_jp[i], prob[i]))
+        probs.append((emotion_names_jp[i], prob[i]))
     return probs, top_emotion, second_emotion
 
 #print(analyze_emotion("今日はいい天気ですね！"))
@@ -53,24 +51,17 @@ def responseGemini(text: str):
     res = model.generate_content(question)
     return res.text
 
-#print(responseGemini("SNSで見た楽しそうな投稿に、なんだか自分だけ置いてけぼり感。"))
+#print(responseGemini("最近体調が優れなくて、何をするにもエネルギーが出ない…。"))
 
 def analyze_gemini(text: str):
     # 感情分析
-    model_h.eval()
-    tokens = tokenizer(text, truncation=True, max_length=512, return_tensors="pt")
-    preds = model_h(**tokens)
-    prob = np_softmax(preds.logits.cpu().detach().numpy()[0]) 
-    top_emotion = emotion_names_jp[np.argmax(prob)]
-    second_emotion = emotion_names_jp[np.argsort(prob)[-2]]
-    probs = []
-    for i in range(len(prob)):
-        probs = probs.append((emotion_names_jp[i], prob[i]))
+    probs, top_emotion, second_emotion = analyze_emotion(text)
     # Gemini
     question = f"{text}の文章に対して肯定的な慰めやアドバイスを返してください。ただし、文章は2文以内でお願いします。"
     res = model.generate_content(question)
     return probs, top_emotion, second_emotion, res.text
 
+#print(analyze_gemini("最近体調が優れなくて、何をするにもエネルギーが出ない…。"))
 
 # FastAPIアプリケーションの作成
 app = FastAPI()
@@ -78,7 +69,7 @@ app = FastAPI()
 # CORS設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # 必要に応じて許可するオリジンを指定
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -95,20 +86,19 @@ async def root():
 
 # エンドポイント: 感情分析
 @app.post("/analyze_emotion")
-async def analyze_emotion(request: EmotionRequest):
+async def analyze_emotion_endpoint(request: EmotionRequest):
     text = request.text
-    response, prob, top_emotion, second_emotion = analyze_emotion(text)
+    response, top_emotion, second_emotion = analyze_emotion(text)
     return {
         "text": text,
         "response": response,
-        "probability": prob,
         "top_emotion": top_emotion,
         "second_emotion": second_emotion
     }
 
 # エンドポイント: Gemini
 @app.post("/gemini")
-async def responseGemini(request: EmotionRequest):
+async def responseGemini_endpoint(request: EmotionRequest):
     text = request.text
     response = responseGemini(text)
     return {
@@ -118,7 +108,7 @@ async def responseGemini(request: EmotionRequest):
 
 # エンドポイント: 感情分析+Gemini
 @app.post("/analyze_gemini")
-async def analyze_gemini(request: EmotionRequest):
+async def analyze_gemini_endpoint(request: EmotionRequest):
     text = request.text
     probs, top_emotion, second_emotion, response = analyze_gemini(text)
     return {
@@ -131,7 +121,5 @@ async def analyze_gemini(request: EmotionRequest):
 
 # エンドポイント: 感情、投稿の傾向に合わせた応答
 @app.post("/moveMatch_response")
-async def root():
+async def moveMatch_response():
     return {"message": "投稿頻度、感情の動きに合わせた応答を返します"}
-
-    
